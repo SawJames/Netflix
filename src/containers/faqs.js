bf1eed4499a49e1d1692446ff5c6ace90823d04f
Saw <tarmalar@gmail.com>
2021-09-11 14:35:41 +0630

    Add Opt From

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Accordion} from '../components'
+import {OptForm} from '../components'
 import faqsData from '../fixtures/faqs';
 
 export function FaqsContainer() {
@@ -14,6 +15,13 @@ export function FaqsContainer() {
                     </Accordion.Item>
                 ))}
             </Accordion.Frame>
+
+            <OptForm>
+                <OptForm.Input placeholder="Email address"/>
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break/>
+                <OptForm.Text>Ready to watch? Enter your email to create or Restart your membership.</OptForm.Text>
+            </OptForm>
         </Accordion>
     )
 }
