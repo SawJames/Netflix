854975b7be56679e88f02c20e681e976e251d19d
Saw <tarmalar@gmail.com>
2021-09-14 09:23:58 +0630

    User Auth with firebase and custom hook user

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Home, Browse, SignIn, SignUp} from './pages'
-import * as ROUTES from './constants/routes'
+import {Home, Browse, SignIn, SignUp} from './pages';
+import * as ROUTES from './constants/routes';
+import {IsUserRedirect, ProtectedRoute} from './helper/routes'
+import {UseAuthListener} from './hooks'
 
 
 function app() {
+  const {user} = UseAuthListener()
+  console.log(user)
+
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse/>
-      </Route>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn/>
+        </IsUserRedirect>
 
-      <Route exact path="/signin">
-        <SignIn/>
-      </Route>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp/>
+        </IsUserRedirect>
 
-      <Route exact path="/signup">
-        <SignUp/>
-      </Route>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse/>
+        </ProtectedRoute>
 
-      <Route exact path={ROUTES.HOME}>
-        <Home/>
-      </Route>
-      
+        <ProtectedRoute user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home/>
+        </ProtectedRoute>
+
+      </Switch>
     </Router>
   );
 }
